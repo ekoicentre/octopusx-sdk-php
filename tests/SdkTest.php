cca6660033615d2f539777e3fb9cd28c970b5f41
Diyangi <ayotomideaina@gmail.com>
2020-02-28 16:27:12 +0100

    test check credentials

--- tests/SdkTest.php
+++ tests/SdkTest.php
@@ -1,8 +1,21 @@
 <?php
 namespace EIC\OctopusX\Tests;
 
+use EIC\OctopusX\Exception\OctopusXException;
 use PHPUnit\Framework\TestCase;
+use EIC\OctopusX\Sdk as Sdk;
 
-class SdkTest extends TestCase{
+class SdkTest extends TestCase
+{
+    /** @var $sdk  */
+    protected $sdk;
+    public function init(){
+        $this->sdk = new Sdk(['credentials' => ['id' => 1, 'secret' => 'fake-secret-code']]);
+    }
 
+    public function testCheckCredentialsFails()
+    {
+        $this->expectException(OctopusXException::class);
+        $sdk = new Sdk();
+    }
 }
